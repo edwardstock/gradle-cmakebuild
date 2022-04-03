eecfe4991f2463cfec716c5b8de8eb7205e19a42
Eduard Maximovich <edward.vstock@gmail.com>
2022-04-03 04:52:29 +0300

    fix pom for gradle artifact

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -47,43 +47,19 @@ dependencies {
     implementation(gradleApi())
 }
 
+
 publishing {
     publications {
         create<MavenPublication>("release") {
-            from(components.getByName("java"))
+            afterEvaluate {
+//                from(components["java"])
+                from(components["kotlin"])
+            }
             artifact(tasks.getByName("sourcesJar"))
             artifact(tasks.getByName("javadocJar"))
             groupId = project.group as String
             artifactId = project.name
             version = project.version as String
-
-            pom {
-                name.set(project.name)
-                url.set("https://github.com/edwardstock/gradle-cmakebuild")
-                inceptionYear.set("2021")
-                description.set("Gradle plugin helps to build CMake project")
-                scm {
-                    connection.set("scm:git:${pom.url}.git")
-                    developerConnection.set(connection)
-                    url.set(pom.url)
-                }
-                licenses {
-                    license {
-                        name.set("The MIT License")
-                        url.set("https://github.com/edwardstock/gradle-cmakebuild/blob/master/LICENSE")
-                        distribution.set("repo")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("edwardstock")
-                        name.set("Eduard Maximovich")
-                        email.set("edward.vstock@gmail.com")
-                        roles.add("owner")
-                        timezone.set("Europe/Moscow")
-                    }
-                }
-            }
         }
     }
     repositories {
@@ -91,6 +67,39 @@ publishing {
         maven(url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")) {
             credentials.username = findProperty("ossrhUsername") as String?
             credentials.password = findProperty("ossrhPassword") as String?
+        }
+    }
+}
+
+afterEvaluate {
+    publishing.publications.forEach {
+        val pub = it as MavenPublication
+        pub.pom {
+            name.set(project.name)
+            url.set("https://github.com/edwardstock/gradle-cmakebuild")
+            inceptionYear.set("2021")
+            description.set("Gradle plugin helps to build CMake project")
+            scm {
+                connection.set("scm:git:${pub.pom.url.get()}.git")
+                developerConnection.set(connection)
+                url.set(pub.pom.url)
+            }
+            licenses {
+                license {
+                    name.set("The MIT License")
+                    url.set("https://github.com/edwardstock/gradle-cmakebuild/blob/master/LICENSE")
+                    distribution.set("repo")
+                }
+            }
+            developers {
+                developer {
+                    id.set("edwardstock")
+                    name.set("Eduard Maximovich")
+                    email.set("edward.vstock@gmail.com")
+                    roles.add("owner")
+                    timezone.set("Europe/Moscow")
+                }
+            }
         }
     }
 }
